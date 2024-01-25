bd0ee96f97603be5d16675620a34c300151edac4
Chokseys2022 <choksey2022@gmail.com>
2024-01-24 21:16:46 -0800

    README.md added

---
+++ README.md
@@ -0,0 +1,42 @@
+Skill Based Assessment for JavaScript Fundamentals:
+
+Code Structure
+Course Information:
+
+CourseInfo
+id
+name
+Assignment Group:
+
+AssignmentGroup
+id
+name
+course_id
+group_weight
+Assignments:
+
+Assignments
+id
+name
+due_at
+points_possible
+Learner Submissions:
+
+LearnerSubmissions
+learner_id
+assignment_id
+submission
+submitted_at
+score
+
+What is Required
+For each learner, the following information needs to be calculated:
+
+learner_id
+Average score
+Score for Assignment 1
+Score for Assignment 2
+
+Notes
+Late submissions are penalized by deducting 10 percent of the assignment's points possible.
+The code checks if the assignment points possible is zero and throws an error in such cases.
--- index.js
+++ index.js
@@ -1,80 +1,81 @@
-// Info provided
+// The provided course information.
 const CourseInfo = {
   id: 451,
-  name: "Introduction to JavaScript",
-};
-
-//Assignment grp info
-const AssignmentGroup = {
+  name: "Introduction to JavaScript"
+  };
+  
+  // The provided assignment group.
+  const AssignmentGroup = {
   id: 12345,
   name: "Fundamentals of JavaScript",
   course_id: 451,
   group_weight: 25,
   assignments: [
-    {
-      id: 1,
-      name: "Declare a Variable",
-      due_at: "2023-01-25",
-      points_possible: 50,
-    },
-    {
-      id: 2,
-      name: "Write a Function",
-      due_at: "2023-02-27",
-      points_possible: 150,
-    },
-    {
-      id: 3,
-      name: "Code the World",
-      due_at: "3156-11-15",
-      points_possible: 500,
-    },
-  ],
-};
-
-// Learner info
-const LearnerSubmissions = [
   {
-    learner_id: 125,
-    assignment_id: 1,
-    submission: {
-      submitted_at: "2023-01-25",
-      score: 47,
-    },
+  id: 1,
+  name: "Declare a Variable",
+  due_at: "2023-01-25",
+  points_possible: 50
   },
   {
-    learner_id: 125,
-    assignment_id: 2,
-    submission: {
-      submitted_at: "2023-02-12",
-      score: 150,
-    },
+  id: 2,
+  name: "Write a Function",
+  due_at: "2023-02-27",
+  points_possible: 150
   },
   {
-    learner_id: 125,
-    assignment_id: 3,
-    submission: {
-      submitted_at: "2023-01-25",
-      score: 400,
-    },
+  id: 3,
+  name: "Code the World",
+  due_at: "3156-11-15",
+  points_possible: 500
+  }
+  ]
+  };
+  
+  // The provided learner submission data.
+  const LearnerSubmissions = [
+  {
+  learner_id: 125,
+  assignment_id: 1,
+  submission: {
+  submitted_at: "2023-01-25",
+  score: 47
+  }
   },
   {
-    learner_id: 132,
-    assignment_id: 1,
-    submission: {
-      submitted_at: "2023-01-24",
-      score: 39,
-    },
+  learner_id: 125,
+  assignment_id: 2,
+  submission: {
+  submitted_at: "2023-02-12",
+  score: 150
+  }
   },
   {
-    learner_id: 132,
-    assignment_id: 2,
-    submission: {
-      submitted_at: "2023-03-07",
-      score: 140,
-    },
+  learner_id: 125,
+  assignment_id: 3,
+  submission: {
+  submitted_at: "2023-01-25",
+  score: 400
+  }
   },
-];
+  {
+  learner_id: 132,
+  assignment_id: 1,
+  submission: {
+  submitted_at: "2023-01-24",
+  score: 39
+  }
+  },
+  {
+  learner_id: 132,
+  assignment_id: 2,
+  submission: {
+  submitted_at: "2023-03-07",
+  score: 140
+  }
+  }
+  ];
+    
 
 // Adding functions here:
 // Function to process learner data: we factor in the late assignments manually
@@ -141,11 +142,12 @@ function getLearnerData(course, ag, submissions) {
     const count = result[learnerId]?.count || 0;
     const assignmentScore = score / assignment.points_possible;
 
-    result[learnerId] = {
+    x.push({
       id: learnerId,
       avg: (avg * count + assignmentScore) / (count + 1),
-      ...result[learnerId],
+      ...(result[learnerId] || {}),
       [assignment.id]: assignmentScore,
-    };
+    });
+    console.log(x);
 
-comsole.log(result)
+
